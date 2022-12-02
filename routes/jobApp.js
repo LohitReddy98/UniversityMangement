1a916a20f8ef23591a792ff1f3572251849aa64c
Chandra <lohitreddycht@gmail.com>
2022-12-01 23:34:53 -0800

    s

--- routes/jobApp.js
+++ routes/jobApp.js
@@ -16,7 +16,7 @@ router.use(async (req, res, next) => {
 router.get("/jobs/:stuId", async (req, res) => {
     try {
         const res = 'select * from JOBS J,JOB_APP JA WHERE JA.stuId=? and JA.jobId = J.jobId'
-        const r=await pool.query(stm, [req.params.stuId]);
+        const r = await pool.query(stm, [parseInt(req.params.stuId)]);
         res.send(r)
     } catch (err) {
         res
@@ -28,7 +28,7 @@ router.get("/jobs/:stuId", async (req, res) => {
 router.get("/students/:jobId", async (req, res) => {
     try {
         const stm = 'select * from STU_INFO S,JOB_APP JA WHERE JA.jobId=? and JA.stuId = S.stuId'
-        const r=await pool.query(stm, [req.params.jobId]);
+        const r = await pool.query(stm, [req.params.jobId]);
         res.send(r)
     } catch (err) {
         res
