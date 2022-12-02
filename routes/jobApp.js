bd50adcd7188eb0ed65ea5604ed1d5ca0f4dea11
Chandra <lohitreddycht@gmail.com>
2022-12-01 21:46:35 -0800

    a

--- routes/jobApp.js
+++ routes/jobApp.js
@@ -15,9 +15,9 @@ router.use(async (req, res, next) => {
 })
 router.get("/jobs/:stuId", async (req, res) => {
     try {
-        const stm = 'select * from JOBS J,JOB_APP JA WHERE JA.stuId=? and JA.jobId = J.jobId'
-        await pool.query(stm, [req.params.stuId]);
-        res.send('successfully added')
+        const res = 'select * from JOBS J,JOB_APP JA WHERE JA.stuId=? and JA.jobId = J.jobId'
+        const r=await pool.query(stm, [req.params.stuId]);
+        res.send(r)
     } catch (err) {
         res
             .status(500)
@@ -28,8 +28,8 @@ router.get("/jobs/:stuId", async (req, res) => {
 router.get("/students/:jobId", async (req, res) => {
     try {
         const stm = 'select * from STU_INFO S,JOB_APP JA WHERE JA.jobId=? and JA.stuId = S.stuId'
-        await pool.query(stm, [req.params.jobId]);
-        res.send('successfully added')
+        const r=await pool.query(stm, [req.params.jobId]);
+        res.send(r)
     } catch (err) {
         res
             .status(500)
